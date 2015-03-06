f4421fc43b77bbcbc5d1110f060d613aa82256ed
Dan Stoppelman <dstop75@gmail.com>
2015-03-06 10:57:40 -0500

    add url property to Movie object

--- app/js/movie_show.js
+++ app/js/movie_show.js
@@ -1,6 +1,9 @@
+/*global $:false */
 'use strict';
 
-var MovieApp = MovieApp || {};
+var MovieApp = MovieApp || {
+    url: 'http:localhost:3000'
+};
 
 $(document).ready(function(){
   console.log('movie_show loaded');
