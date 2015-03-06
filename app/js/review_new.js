bd9aa92e41c7f2e902af4b11bf865fd0f4576564
Doug <djmacdonald@uchicago.edu>
2015-03-06 15:29:09 -0500

    can create a new review, then it refrshes the page

--- app/js/review_new.js
+++ app/js/review_new.js
@@ -7,30 +7,25 @@ var MovieApp = MovieApp || {
 
 MovieApp.createReview = function(event){
   var id = MovieApp.getParams();
-  if (event.preventDefault) {
-    event.preventDefault();
-  }
-  console.log(MovieApp.url + '/movies' + id + '/reviews');
-  // $.ajax({
-  //   url: MovieApp.url + '/movies' + id + '/reviews',
-  //   type: 'POST',
-  //   data: {
-  //     movie: {
-  //       title: $('input#movieTitle').val(),
-  //       gross: $('input#movieGross').val(),
-  //       release: $('input#movieRelease').val(),
-  //       rating: $('input#movierating').val(),
-  //       description: $('textarea#movieDescription').val()
-  //     }
-  //   }
-  // }).done(function(data){
-  //   MovieApp.goToMovie(data);
-  // }).fail();
+  event.preventDefault();
+  $.ajax({
+    url: MovieApp.url + '/movies/' + id + '/reviews',
+    type: 'POST',
+    data: {
+      review: {
+        comment: $('textarea#reviewComment').val(),
+        stars: $('input#reviewStars').val(),
+        reviewer: $('input#reviewReviewer').val()
+      }
+    }
+  }).done(function(){
+    MovieApp.showReviews();
+  }).fail();
 };
 
-// MovieApp.goToMovie = function(data){
-//   window.location.href = '/show.html?movie=' + data.id;
-// };
+MovieApp.showReviews = function(){
+  location.reload();
+};
 
 $(document).ready(function(){
   console.log('review_new loaded');
