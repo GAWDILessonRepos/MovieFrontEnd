da401f78668a2c8d8a9d9ddf337f2c5f0e2c93db
abroccoli <acbrocco@syr.edu>
2015-03-06 10:29:20 -0500

    created js files

---
+++ app/js/movie_show.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var MovieApp = MovieApp || {};
+
+$(document).ready(function(){
+  console.log('movie_show loaded');
+});
---
+++ app/js/review_new.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var MovieApp = MovieApp || {};
+
+$(document).ready(function(){
+  console.log('review_new loaded');
+});
