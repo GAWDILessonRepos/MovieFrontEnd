1b5bda540743b5a3e927a608eec1bc15688cec5b
Doug <djmacdonald@uchicago.edu>
2015-03-06 15:16:12 -0500

    added submit button and home button to show page

--- app/js/review_new.js
+++ app/js/review_new.js
@@ -5,6 +5,34 @@ var MovieApp = MovieApp || {
     url: 'http://localhost:3000'
 };
 
+MovieApp.createReview = function(event){
+  var id = MovieApp.getParams();
+  if (event.preventDefault) {
+    event.preventDefault();
+  }
+  console.log(MovieApp.url + '/movies' + id + '/reviews');
+  // $.ajax({
+  //   url: MovieApp.url + '/movies' + id + '/reviews',
+  //   type: 'POST',
+  //   data: {
+  //     movie: {
+  //       title: $('input#movieTitle').val(),
+  //       gross: $('input#movieGross').val(),
+  //       release: $('input#movieRelease').val(),
+  //       rating: $('input#movierating').val(),
+  //       description: $('textarea#movieDescription').val()
+  //     }
+  //   }
+  // }).done(function(data){
+  //   MovieApp.goToMovie(data);
+  // }).fail();
+};
+
+// MovieApp.goToMovie = function(data){
+//   window.location.href = '/show.html?movie=' + data.id;
+// };
+
 $(document).ready(function(){
   console.log('review_new loaded');
+  $('#reviewForm').submit(MovieApp.createReview);
 });
