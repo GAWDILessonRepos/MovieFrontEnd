2f0841e49793019971ec48a2480504482d70d4ca
Dan Stoppelman <dstop75@gmail.com>
2015-03-06 14:19:45 -0500

    establish basic functionality to show all movies on index page

--- app/js/movie_index.js
+++ app/js/movie_index.js
@@ -1,10 +1,31 @@
+// DS
 /*global $:false */
 'use strict';
 
 var MovieApp = MovieApp || {
-    url: 'http://localhost:3000'
+  url: 'http://localhost:3000'
+};
+
+MovieApp.createHTML = function(movie){
+  var html = '<li>' + movie.title + '</li>';
+  $('ul#movies').append(html);
+};
+
+MovieApp.renderMovie = function(movie){
+  MovieApp.createHTML(movie);
+};
+
+MovieApp.renderMovies = function(movies){
+  movies.forEach(MovieApp.renderMovie);
+};
+
+MovieApp.showMovies = function(){
+  $.get(MovieApp.url, function(data) {
+    MovieApp.renderMovies(data);
+  });
 };
 
 $(document).ready(function(){
   console.log('movie_index loaded');
+  MovieApp.showMovies();
 });
