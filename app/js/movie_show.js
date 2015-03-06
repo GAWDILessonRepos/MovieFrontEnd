e8cf80395f7eea731f2feba0d58461aaa9cdff32
abroccoli <acbrocco@syr.edu>
2015-03-06 14:43:57 -0500

    rendering html for single movie and its reviews

--- app/js/movie_show.js
+++ app/js/movie_show.js
@@ -13,15 +13,22 @@ MovieApp.getParams = function(){
 
 MovieApp.getMovie = function(){
 	var id = MovieApp.getParams();
-	console.log(MovieApp.url + '/movies/' + id);
-	// $.ajax({
-	// 	url: MovieApp.url + '/movies/' + id,
-	// 	type: 'GET'
-	// }).done(function(data){
-	// 	console.log(data);
-	// }).fail(function(jqXHR, textStatus, errorThrown){
-	// 	console.log('error!!!');
-	// });
+	$.ajax({
+		url: MovieApp.url + '/movies/' + id,
+		type: 'GET'
+	}).done(function(data){
+		MovieApp.renderHTML(data);
+	}).fail(function(jqXHR, textStatus, errorThrown){
+		console.log('error!!!');
+	});
+};
+
+MovieApp.renderHTML = function(movie){
+	var html = '<div class="fullMovie"><h3 class="movieShowTitle">'+movie.title+'</h3><h4 class="movieShowRating">'+movie.rating+'</h4><h4 class="movieShowRelease">'+movie.release+'</h4><p class="movieShowDescription">'+movie.description+'</p></div>';
+		for (var i=0; i<movie.reviews.length; i++){
+		html += '<div class="movieReviews"><p class="reviewShowReviewer">'+movie.reviews[i].reviewer+'</p><p class="reviewShowStars">'+movie.reviews[i].stars+'</p><p class="reviewShowComment">'+movie.reviews[i].comment+'</p></div>';
+		}
+	$('#movieShow').html(html);	
 };
 
 $(document).ready(function(){
