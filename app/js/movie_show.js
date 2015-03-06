fa356019aa747739bc0c8ba49551ff5508693b01
abroccoli <acbrocco@syr.edu>
2015-03-06 11:23:53 -0500

    created get params and get movie functions

--- app/js/movie_show.js
+++ app/js/movie_show.js
@@ -5,6 +5,25 @@ var MovieApp = MovieApp || {
     url: 'http://localhost:3000'
 };
 
+MovieApp.getParams = function(){
+	var params = window.location.search;
+	var re = /\d/;
+	return re.exec(params);
+};
+
+MovieApp.getMovie = function(){
+	var id = getParams();
+	$.ajax({
+		url: MovieApp.url + '/movies/' + id,
+		type: 'GET'
+	}).done(function(data){
+		console.log(data)
+	}).fail(function(jqXHR, textStatus, errorThrown){
+		console.log('error!!!')
+	});
+};
+
 $(document).ready(function(){
   console.log('movie_show loaded');
+  MovieApp.getMovie();
 });
